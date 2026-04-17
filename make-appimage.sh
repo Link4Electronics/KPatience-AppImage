2cf2a5438094bab6cd7b2c061df1baa0cc2c09cb
Link4Electronics <link4electronics@gmail.com>
2026-04-17 02:57:35 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -15,7 +15,7 @@ export DEPLOY_QT=1
 export QT_DIR=qt6
 
 # Deploy dependencies
-quick-sharun /usr/bin/kpat /usr/share/kpat
+quick-sharun /usr/bin/kpat /usr/share/kpat /usr/share/knsrcfiles
 
 # Additional changes can be done in between here
 
